f940beff87e3f42ee155ddbb8e887632dbc4985e
Emreskr <emresekeroglu12@gmail.com>
2023-05-28 12:50:01 +0200

    Added Label Encoder

--- ml-project.py
+++ ml-project.py
@@ -7,19 +7,32 @@ import pandas as pd
 #2.data preprocessing
 #2.1. data loading
 data = pd.read_csv('honda_sell_data.csv')
-
+year = data.iloc[:,0]
+model = data.iloc[:, 2]
+price = data.iloc[:, 4].values
+x = pd.concat([model, year], axis=1)
 #test
 print(data)
 
-x = data.iloc[:,0:4].values
-y = data.iloc[:,4].values 
-print(y)
+#%%
+#Label encoding
+from sklearn import preprocessing
 
+le = preprocessing.LabelEncoder()
+x.iloc[:, 0] = le.fit_transform(data.iloc[:,2])
+
+#odel = model.reshape(-1,1)
+#OneHotEncoder
+#ohe = preprocessing.OneHotEncoder()
+#model = ohe.fit_transform(model).toarray()
+y = price
+
+#%%
 #data splitting
 from sklearn.model_selection import train_test_split
 
 x_train, x_test,y_train,y_test = train_test_split(x,y,test_size=0.33, random_state=0)
-
+#%%
 #data scaling
 from sklearn.preprocessing import StandardScaler
 
@@ -28,14 +41,15 @@ sc=StandardScaler()
 X_train = sc.fit_transform(x_train)
 X_test = sc.transform(x_test)
 
-
+#%%
+#Logistic regression
 from sklearn.linear_model import LogisticRegression
 logr = LogisticRegression(random_state=0)
 logr.fit(X_train,y_train)
 
 y_pred = logr.predict(X_test)
-print(y_pred)
-print(y_test)
+#print(y_pred)
+#print(y_test)
 
 
 from sklearn.metrics import confusion_matrix
@@ -43,10 +57,11 @@ cm = confusion_matrix(y_test,y_pred)
 print(cm)
 
 
-
+#%%
+#K-Nearest Neighbors
 from sklearn.neighbors import KNeighborsClassifier
 
-knn = KNeighborsClassifier(n_neighbors=1, metric='minkowski')
+knn = KNeighborsClassifier(n_neighbors=3, metric='minkowski')
 knn.fit(X_train,y_train)
 
 y_pred = knn.predict(X_test)
@@ -55,7 +70,8 @@ cm = confusion_matrix(y_test,y_pred)
 print(cm)
 
 
-
+#%%
+#Support Vector
 from sklearn.svm import SVC
 svc = SVC(kernel='rbf')
 svc.fit(X_train,y_train)
@@ -66,6 +82,8 @@ cm = confusion_matrix(y_test,y_pred)
 print('SVC')
 print(cm)
 
+#%%
+#Gaussian Naive Bayes
 from sklearn.naive_bayes import GaussianNB
 gnb = GaussianNB()
 gnb.fit(X_train, y_train)
@@ -75,6 +93,8 @@ y_pred = gnb.predict(X_test)
 cm = confusion_matrix(y_test,y_pred)
 print('GNB')
 
+#%%
+#Decision Tree
 from sklearn.tree import DecisionTreeClassifier
 
 dtc = DecisionTreeClassifier(criterion = 'entropy')
@@ -86,6 +106,8 @@ cm = confusion_matrix(y_test, y_pred)
 print('DTC')
 print(cm)
 
+#%%
+#Random Forest
 from sklearn.ensemble import RandomForestClassifier
 rfc = RandomForestClassifier(n_estimators=15, criterion = 'gini')
 
